5631bc0532cf2e7f49e84c6fa1fdbf82cea8264d
Deepu <d7800278@gmail.com>
2026-05-22 10:07:37 +0530

    changes

---
+++ .vscode/function.js
@@ -0,0 +1,22 @@
+function welcome(){
+    console.log('Hi')
+}
+welcome();
+
+function add(x,y){            // here x,y are arguments
+    console.log(x+y);  
+}
+add(2,3)      // here 2,3 are  parameters
+
+function add1(x,y){
+    console.log(x+y);
+    return;
+}
+const data=add(4,5);
+console.log(data)
+
+//Arrow function
+//syntax  nameof varaiabledeclare varaibalename=() =>{};
+
+var username=()=>'vasantha'+'lakshmi';
+console.log(username())
